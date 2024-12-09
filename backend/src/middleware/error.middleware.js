ef85713ca42f6d13900634aad36ba3763e12e31e
mrizkyff <muhamadrizkyff@gmail.com>
2024-12-09 11:27:51 +0700

    response error & middleware

---
+++ backend/src/middleware/error.middleware.js
@@ -0,0 +1,37 @@
+const ResponseError = require('../response/error.response');
+
+const errorMiddleware = async (err, req, res, next) => {
+    if (!err) {
+        next();
+        return;
+    }
+    console.log(`Error: ${err}`);
+    if (err instanceof ResponseError) {
+        let message = err.message;
+        res.status(err.statusCode).json({
+            Message: {
+                Code: err.statusCode,
+                Text: message
+            },
+            Data: {
+                Message: err.details
+            },
+            Type: req.originalUrl
+        }).end();
+    }
+    else {
+        res.status(500).json({
+            Message: {
+                Code: 500,
+                Text: "Internal Server Error"
+            },
+            Data: {
+                Message: err.message
+            },
+            Type: req.originalUrl
+        }).end();
+
+    }
+}
+
+module.exports = {errorMiddleware};
