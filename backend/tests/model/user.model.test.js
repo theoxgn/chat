2ca4483a88d2ab3a8fd08a0c9463ac0adc2fc6bf
mrizkyff <muhamadrizkyff@gmail.com>
2024-12-10 09:31:01 +0700

    init tests & jest

---
+++ backend/tests/model/user.model.test.js
@@ -0,0 +1,60 @@
+const {sequelize, User} = require('../../models');
+const {Op} = require('sequelize');
+
+beforeEach(async () => {
+    await User.destroy({
+        where: {
+            email: {
+                [Op.like]: 'test%'
+            }
+        }
+    });
+});
+
+beforeAll(async () => {
+    await sequelize.sync();
+});
+
+afterAll(async () => {
+    await sequelize.close();
+});
+
+describe('User Model', () => {
+    it('should create a new user', async () => {
+        const userData = {
+            username: 'testuser',
+            companyName: 'testcompany',
+            email: 'testuser@example.com',
+            password: 'password123',
+            profilePicture: "https://example.com",
+            role: "buyer",
+            status: "active",
+            nameChangesCount: 0,
+            lastNameChange: new Date(),
+            isVerified: false,
+            referralCode: "MTX98271",
+            lastSeen: new Date()
+        };
+
+        try {
+            const user = await User.create(userData);
+        } catch (e) {
+            console.log(e);
+        }
+
+        // expect(user.id).toBeDefined();
+        // expect(user.username).toBe(userData.username);
+        // expect(user.companyName).toBe(userData.companyName);
+        // expect(user.email).toBe(userData.email);
+        // expect(user.password).toBe(userData.password);
+        // expect(user.profilePicture).toBe(userData.profilePicture);
+        // expect(user.role).toBe(userData.role);
+        // expect(user.status).toBe(userData.status);
+        // expect(user.nameChangesCount).toBe(userData.nameChangesCount);
+        // expect(user.lastNameChange).toBe(userData.lastNameChange);
+        // expect(user.isVerified).toBe(userData.isVerified);
+        // expect(user.referralCode).toBe(userData.referralCode);
+        // expect(user.lastSeen).toBe(userData.lastSeen);
+        // expect(user.createdAt).toBeDefined();
+    });
+});
---
+++ backend/tests/sample.test.js
@@ -0,0 +1,5 @@
+describe('Sample test', () => {
+    it('should be ok', async () => {
+        expect(1).toBe(1);
+    });
+});
