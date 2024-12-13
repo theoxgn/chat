4aed4fb6d42e14bf56efe8352674c7f2b167ddf6
mrizkyff <muhamadrizkyff@gmail.com>
2024-12-13 15:05:04 +0700

    forward message ok

--- backend/src/route/message.route.js
+++ backend/src/route/message.route.js
@@ -9,9 +9,9 @@ messagesRouter.get('/api/messages/:roomId', MessageController.getMessagesByRoomI
 messagesRouter.post('/api/messages', MessageController.createMessage);
 messagesRouter.post('/api/messages/read', MessageController.readMessage);
 messagesRouter.post('/api/messages/reply', MessageController.replyMessage);
+messagesRouter.post('/api/messages/forward', MessageController.forwardMessage);
 messagesRouter.get('/api/messages/unread/:userId', MessageController.getUnreadMessagesCount);
 messagesRouter.get('/api/messages/search/:roomId', MessageController.searchMessageInRoom);
 messagesRouter.delete('/api/messages/:messageId', MessageController.deleteMessage);
-messagesRouter.post('/api/messages/forward', MessageController.forwardMessage);
 
 module.exports = messagesRouter;
