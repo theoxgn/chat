82250a8be405674498562720a353192825485623
mrizkyff <muhamadrizkyff@gmail.com>
2024-12-14 11:17:33 +0700

    update todo & snake case to camelCase in chats

--- backend/src/service/chat.service.js
+++ backend/src/service/chat.service.js
@@ -148,26 +148,25 @@ class ChatService {
         // * Opposite is role of other user in chatroom
         const chats = await db.sequelize.query(
             `
-                select cr.id                               as id,
-                       initiator.id                        as initiator_id,
-                       initiator.username                  as initiator_username,
-                       cr.initiator_role                   as initiator_role,
-                       recipient.id                        as recipient_id,
-                       recipient.username                  as recipient_username,
-                       cr.recipient_role                   as recipient_role,
-                       M.content                           as last_message_content,
-                       M.created_at                        as last_message_created_at,
-                       M.message_type                      as last_message_type,
-                       M.status                            as last_message_status,
-                       PC.created_at                       as pinned_at,
-                       CM.name                             as menu_name,
-                       CSM.name                            as sub_menu_name,
+                select cr.id                               as "id",
+                       initiator.id                        as "initiatorId",
+                       initiator.username                  as "initiatorUsername",
+                       cr.initiator_role                   as "initiatorRole",
+                       recipient.id                        as "recipientId",
+                       recipient.username                  as "recipientUsername",
+                       cr.recipient_role                   as "recipientRole",
+                       M.content                           as "lastMessageContent",
+                       M.created_at                        as "lastMessageCreatedAt",
+                       M.message_type                      as "lastMessageType",
+                       M.status                            as "lastMessageStatus",
+                       PC.created_at                       as "pinnedAt",
+                       CM.name                             as "menuName",
+                       CSM.name                            as "subMenuName",
                        (SELECT COUNT(*)
                         FROM public."Messages" unread
                         WHERE unread.chat_room_id = cr.id
                           AND (unread.status = 'delivered' or unread.status = 'sent')
-                          AND unread.sender_id != :userId) as unread_count
-
+                          AND unread.sender_id != :userId) as "unreadCount"
                 from "ChatRooms" cr
                          left join public."Users" initiator on cr.initiator = initiator.id
                          left join public."Users" recipient on cr.recipient = recipient.id
@@ -249,25 +248,25 @@ class ChatService {
         const chats = await db.sequelize.query(
             `
                 select cr.id              as id,
-                       initiator.id       as initiator_id,
-                       initiator.username as initiator_username,
-                       cr.initiator_role  as initiator_role,
-                       recipient.id       as recipient_id,
-                       recipient.username as recipient_username,
-                       cr.recipient_role  as recipient_role,
-                       PC.created_at      as pinned_at,
+                       initiator.id       as "initiatorId",
+                       initiator.username as "initiatorUsername",
+                       cr.initiator_role  as "initiatorRole",
+                       recipient.id       as "recipientId",
+                       recipient.username as "recipientUsername",
+                       cr.recipient_role  as "recipientRole",
+                       PC.created_at      as "pinnedAt",
                        json_build_object(
-                               'last_message_content', M.content,
-                               'last_message_created_at', M.created_at,
-                               'last_message_type', M.message_type,
-                               'last_message_status', M.status,
-                               'menu_name', CM.name,
-                               'sub_menu_name', CSM.name,
-                               'unread_count', (SELECT COUNT(*)
-                                                FROM public."Messages" unread
-                                                WHERE unread.chat_room_id = cr.id
-                                                  AND (unread.status = 'delivered' or unread.status = 'sent')
-                                                  AND unread.sender_id != :userId)
+                               'lastMessageContent', M.content,
+                               'lastMessageCreatedAt', M.created_at,
+                               'lastMessageType', M.message_type,
+                               'lastMessageStatus', M.status,
+                               'menuName', CM.name,
+                               'subMenuName', CSM.name,
+                               'unreadCount', (SELECT COUNT(*)
+                                               FROM public."Messages" unread
+                                               WHERE unread.chat_room_id = cr.id
+                                                 AND (unread.status = 'delivered' or unread.status = 'sent')
+                                                 AND unread.sender_id != :userId)
                        )                  as chats
 
                 from "ChatRooms" cr
